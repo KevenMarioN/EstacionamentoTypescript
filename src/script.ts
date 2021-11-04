cc60dcd0e0d8efc0b838aeef1d21b7224d2f251b
KevenMarioN <kevenmario.n.r@gmail.com>
2021-11-03 23:27:36 -0300

    feat: implementação de logo e nome, implemtanção da função para adicionar carro na tabela

--- src/script.ts
+++ src/script.ts
@@ -1,33 +1,50 @@
+interface Veiculo {
+	modelo: string;
+	placa: string;
+	entrada?: Date;
+}
 (() => {
-	const $ = (query : string) : HTMLInputElement | null => document.querySelector(query)
+	const $ = (query: string): HTMLInputElement | null => document.querySelector(query)
 
-	function patio(){
-		function ler () {
-
-		}
-		function adicionar(){
-			const modeloVeiculo = $("body > div > div:nth-child(1) > input")?.value;
-			const placaVeiculo = $("body > div > div:nth-child(2) > input")?.value;
-			console.log(modeloVeiculo,placaVeiculo)
-			if(!modeloVeiculo || !placaVeiculo){
-				alert("Os campos nome e placa são obrigatorios!")
-				return;
-		}
-		}
-		function remover(){
+	function patio() {
+		function ler() {
 
 		}
-		function salvar(){
+		function adicionar({ modelo, placa, entrada = new Date()}: Veiculo) {
+			const row = document.createElement("tr");
+
+			row.innerHTML = `
+			<td>${modelo}</td>
+			<td>${placa}</td>
+			<td>${entrada}</td>
+			<td>
+			<button class="btn btn-danger" data-placa="${placa}">X</button>
+			</td>
+			`;
+			$("#patio")?.appendChild(row)
+		}
+		function remover() {
 
 		}
-		function render(){
+		function salvar() {
 
 		}
+		function render() {
 
-		return { ler,adicionar,remover,salvar,render}
+		}
+		return { ler, adicionar, remover, salvar, render 
 	}
+}
 
-	$("#cadastra")?.addEventListener("click",() => {
-		patio().adicionar();
+	$("#cadastra")?.addEventListener("click", () => {
+
+		const modeloVeiculo = $("body > div > div:nth-child(1) > input")?.value;
+		const placaVeiculo = $("body > div > div:nth-child(2) > input")?.value;
+		if (!modeloVeiculo || !placaVeiculo) {
+			alert("Os campos nome e placa são obrigatorios!")
+			return;
+		} else {
+			patio().adicionar({ modelo: modeloVeiculo, placa: placaVeiculo })
+		}
 	});
 })();
